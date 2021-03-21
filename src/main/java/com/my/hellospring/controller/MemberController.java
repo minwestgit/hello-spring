df21221fd33455872adaf287bcd66b3a864d4d88
minwestgit <minwest61@gmail.com>
2021-03-21 21:52:27 +0900

    웹 MVC 개발, 데이터베이스 설치

---
+++ src/main/java/com/my/hellospring/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.my.hellospring.controller;
+
+import com.my.hellospring.domain.Member;
+import com.my.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // 회원 등록 폼
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 회원 등록 기능
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
