e055f5bedabce00aaa3ce5c4cdec88ab49021b97
minwestgit <minwest61@gmail.com>
2021-03-22 22:55:11 +0900

    스프링 JdbcTemplate

---
+++ src/main/java/com/my/hellospring/SpringConfig.java
@@ -0,0 +1,26 @@
+package com.my.hellospring;
+
+import com.my.hellospring.repository.JdbcMemberRepository;
+import com.my.hellospring.repository.JdbcTemplateMemberRepository;
+import com.my.hellospring.repository.MemberRepository;
+import com.my.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+}
