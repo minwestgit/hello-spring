29837ae322f2a599ed93df0cc933095ff1f3887e
minwestgit <minwest61@gmail.com>
2021-03-23 21:44:24 +0900

    스프링 JdbcTemplate + JPA

--- src/main/java/com/my/hellospring/SpringConfig.java
+++ src/main/java/com/my/hellospring/SpringConfig.java
@@ -2,25 +2,29 @@ package com.my.hellospring;
 
 import com.my.hellospring.repository.JdbcMemberRepository;
 import com.my.hellospring.repository.JdbcTemplateMemberRepository;
+import com.my.hellospring.repository.JpaMemberRepository;
 import com.my.hellospring.repository.MemberRepository;
 import com.my.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    //private final DataSource dataSource;
+    private EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource); // 순수 Jdbc
+        // return new JdbcTemplateMemberRepository(dataSource); // Spring Jdbc Template
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/com/my/hellospring/repository/JdbcMemberRepository.java
+++ src/main/java/com/my/hellospring/repository/JdbcMemberRepository.java
@@ -18,20 +18,25 @@ public class JdbcMemberRepository implements MemberRepository {
     @Override
     public Member save(Member member) {
         String sql = "insert into member(name) values(?)";
+
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
+
         try {
             conn = getConnection();
+
             pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, member.getName());
             pstmt.executeUpdate();
+
             rs = pstmt.getGeneratedKeys();
             if (rs.next()) {
                 member.setId(rs.getLong(1));
             } else {
                 throw new SQLException("id 조회 실패");
             }
+
             return member;
         } catch (Exception e) {
             throw new IllegalStateException(e);
@@ -43,13 +48,17 @@ public class JdbcMemberRepository implements MemberRepository {
     @Override
     public Optional<Member> findById(Long id) {
         String sql = "select * from member where id = ?";
+
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
+
         try {
             conn = getConnection();
+
             pstmt = conn.prepareStatement(sql);
             pstmt.setLong(1, id);
+
             rs = pstmt.executeQuery();
             if(rs.next()) {
                 Member member = new Member();
@@ -69,12 +78,16 @@ public class JdbcMemberRepository implements MemberRepository {
     @Override
     public List<Member> findAll() {
         String sql = "select * from member";
+
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
+
         try {
             conn = getConnection();
+
             pstmt = conn.prepareStatement(sql);
+
             rs = pstmt.executeQuery();
             List<Member> members = new ArrayList<>();
             while(rs.next()) {
