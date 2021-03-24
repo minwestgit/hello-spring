9b116cdd8454ef787cf0ef944b353ffadce644c9
minwestgit <minwest61@gmail.com>
2021-03-24 21:39:25 +0900

    스프링 데이터 JPA, AOP

--- src/main/java/com/my/hellospring/SpringConfig.java
+++ src/main/java/com/my/hellospring/SpringConfig.java
@@ -1,30 +1,46 @@
 package com.my.hellospring;
 
-import com.my.hellospring.repository.JdbcMemberRepository;
-import com.my.hellospring.repository.JdbcTemplateMemberRepository;
-import com.my.hellospring.repository.JpaMemberRepository;
+import com.my.hellospring.aop.TimeTraceAop;
 import com.my.hellospring.repository.MemberRepository;
 import com.my.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    //private final DataSource dataSource;
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource); // 순수 Jdbc
-        // return new JdbcTemplateMemberRepository(dataSource); // Spring Jdbc Template
-        return new JpaMemberRepository(em);
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+
+//    private final DataSource dataSource;
+//    private EntityManager em;
+
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        // return new MemoryMemberRepository();
+//        // return new JdbcMemberRepository(dataSource); // 순수 Jdbc
+//        // return new JdbcTemplateMemberRepository(dataSource); // Spring Jdbc Template
+//        return new JpaMemberRepository(em);
+//    }
+
 }
